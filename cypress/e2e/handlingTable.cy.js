e45cc1727e809829aaadfb2d14c62651631c079f
Mashruf Rohon <mashrufrohon@gmail.com>
2025-05-24 10:48:24 +0600

    handling table

--- cypress/e2e/handlingTable.cy.js
+++ cypress/e2e/handlingTable.cy.js
@@ -1,16 +1,17 @@
 describe("Handling table",()=>{
 
-    beforeEach("",()=>{
-        cy.visit("https://vinothqaacademy.com/webtable/");
-    })
     it("Number of rows and column",()=>{
+        cy.visit("https://vinothqaacademy.com/webtable/");
         cy.get("thead>tr>th").should("have.length",6);
         cy.get("tbody>tr").should("have.length",11);
+        
     })
     it("Check cell data",()=>{
+        cy.visit("https://vinothqaacademy.com/webtable/");
         cy.get("tbody>tr:nth-child(3)>td:nth-child(4)").should("contain","vinoth.r@example.com");
     })
     it("Read data from all the rows and columns",()=>{
+        cy.visit("https://vinothqaacademy.com/webtable/");
         cy.get("tbody>tr").each((row)=>{
             cy.wrap(row).find("td").each((column)=>{
                 cy.log(column.text());
